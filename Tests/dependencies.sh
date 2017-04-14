c2e84f84b693d0e05f0c159cca209be5a0052225
Grégoire Humeau <gregoire@appventus.com>
2017-04-14 16:09:44 +0200

    Update elasticsearch donwload url

--- Tests/dependencies.sh
+++ Tests/dependencies.sh
@@ -1,4 +1,4 @@
-wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/1.7.2/elasticsearch-1.7.2.tar.gz
+wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.2.tar.gz
 tar -xvf elasticsearch-1.7.2.tar.gz
 nohup elasticsearch-1.7.2/bin/elasticsearch > /dev/null 2>&1 &
 sleep 10 && wget --waitretry=5 --retry-connrefused -v http://127.0.0.1:9200/
