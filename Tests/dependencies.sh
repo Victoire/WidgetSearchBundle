45343fd91694e1ee6a852fe480dd525d7471e089
Grégoire Humeau <gregoire@appventus.com>
2017-04-18 11:36:56 +0200

    Require title-widget in dependencies.sh

--- Tests/dependencies.sh
+++ Tests/dependencies.sh
@@ -2,3 +2,4 @@ wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.2
 tar -xvf elasticsearch-1.7.2.tar.gz
 nohup elasticsearch-1.7.2/bin/elasticsearch > /dev/null 2>&1 &
 sleep 10 && wget --waitretry=5 --retry-connrefused -v http://127.0.0.1:9200/
+php -d memory_limit=-1 /usr/local/bin/composer require victoire/title-widget dev-master
